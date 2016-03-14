df4eabef1e3aee19a2bfe61a01d28b1aee8cceb8
allenchen <allenchen@nine-yi.com>
2016-03-15 00:29:42 +0800

    撰寫第二個測試案例的Production Code

--- BookStoreService/HarryPotterService.cs
+++ BookStoreService/HarryPotterService.cs
@@ -10,7 +10,25 @@ namespace BookStoreService
         public object CalculatePrice(List<HarryPortterBook> books)
         {
             var totalPrice = 0;
-            totalPrice = books[0].Price;
+
+            switch(books.Count())
+            {
+                case 1:
+                    totalPrice = books[0].Price;
+                    break;
+                case 2:
+
+                    if (books[0].Name == books[1].Name)
+                    {
+                        totalPrice = books.Sum(x => x.Price);
+                    }
+                    else
+                    {
+                        totalPrice = (int)(books.Sum(x => x.Price) * 0.95);
+                    }
+
+                    break;
+            }
 
             return totalPrice;
         }
