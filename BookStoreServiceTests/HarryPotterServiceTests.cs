21658e83e2651d46d35ebd478f73cf469196429d
allenchen <allenchen@nine-yi.com>
2016-03-15 01:59:58 +0800

    建立第六個測試案例: 一二集各買了一本, 第三集買了兩本, 價格應為100*3*0.9 + 100 = 370

--- BookStoreServiceTests/HarryPotterServiceTests.cs
+++ BookStoreServiceTests/HarryPotterServiceTests.cs
@@ -109,5 +109,26 @@ namespace BookStoreServiceTests
             //// Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void 一二集各買了一本_第三集買了兩本_價格應為370()
+        {
+            //// Arrange
+            var books = new List<HarryPortterBook>();
+            books.Add(new HarryPortterBook { Name = "哈利波特第一集", Episode = 1, Price = 100 });
+            books.Add(new HarryPortterBook { Name = "哈利波特第二集", Episode = 2, Price = 100 });
+            books.Add(new HarryPortterBook { Name = "哈利波特第三集", Episode = 3, Price = 100 });
+            books.Add(new HarryPortterBook { Name = "哈利波特第三集", Episode = 3, Price = 100 });
+            
+            var target = new HarryPotterService();
+
+            var expected = 370;
+
+            //// Act
+            var actual = target.CalculatePrice(books);
+
+            //// Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
