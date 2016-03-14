2595fb91179f0c69dd74be62cf14ff16829eb12a
allenchen <allenchen@nine-yi.com>
2016-03-15 00:45:57 +0800

    撰寫第三個測試案例Production Code

--- BookStoreService/HarryPotterService.cs
+++ BookStoreService/HarryPotterService.cs
@@ -12,9 +12,16 @@ namespace BookStoreService
             var totalPrice = 0;
             totalPrice = books.Sum(x => x.Price);
 
-            if(books.Count() == 2 && books[0].Name != books[1].Name)
+            if (books.Count() == 2 && books[0].Name != books[1].Name)
             {
                 totalPrice = (int)(totalPrice * 0.95);
+            }
+            else if (books.Count() == 3
+                            && books[0].Name != books[1].Name
+                            && books[1].Name != books[2].Name
+                            && books[0].Name != books[2].Name)
+            {
+                totalPrice = (int)(totalPrice * 0.9);
             }
 
             return totalPrice;
