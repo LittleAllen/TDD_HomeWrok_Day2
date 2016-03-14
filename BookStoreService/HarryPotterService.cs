81988e03213b4bb040a10a701b7f370aa5175056
allenchen <allenchen@nine-yi.com>
2016-03-15 01:59:34 +0800

    重構: 取得折扣抽成方法

--- BookStoreService/HarryPotterService.cs
+++ BookStoreService/HarryPotterService.cs
@@ -14,27 +14,42 @@ namespace BookStoreService
 
             if (books.Count() == books.GroupBy(x => x.Name).Count())
             {
-                switch (books.Count())
-                {
-                    case 2:
-                        totalPrice = (int)(totalPrice * 0.95);
-                        break;
-
-                    case 3:
-                        totalPrice = (int)(totalPrice * 0.9);
-                        break;
-
-                    case 4:
-                        totalPrice = (int)(totalPrice * 0.8);
-                        break;
-
-                    case 5:
-                        totalPrice = (int)(totalPrice * 0.75);
-                        break;
-                }
+                var discount = this.GetDiscount(books.Count());
+                totalPrice = (int)(totalPrice * discount);
             }
 
             return totalPrice;
+        }
+
+        /// <summary>
+        /// 取得折扣
+        /// </summary>
+        /// <param name="discountType">折扣類別</param>
+        /// <returns>折扣</returns>
+        private double GetDiscount(int discountType)
+        {
+            double discount = 1;
+
+            switch (discountType)
+            {
+                case 2:
+                    discount = 0.95;
+                    break;
+
+                case 3:
+                    discount = 0.9;
+                    break;
+
+                case 4:
+                    discount = 0.8;
+                    break;
+
+                case 5:
+                    discount = 0.75;
+                    break;
+            }
+
+            return discount;
         }
     }
 }
